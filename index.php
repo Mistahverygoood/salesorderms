e7feae7aa8959e58364c20ab9438a565a4fa285f
MistahVeryGood <khurrissa@hotmail.com>
2018-02-12 20:49:53 +0800

    updated files

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
 <!--    -->
@@ -15,7 +16,17 @@
             <div id="navwrapper">
                 <a class="underline" href="#"><div id="menu1" class="menu1">Dashboard</div></a>
                 <a class="underline" href="#"><div id="menu1" class="menu1">Customers</div></a>
-                <a class="underline" href="#"><div id="menu1" class="menu1">Sales</div></a>
+                  <div class="dropdown">
+                      <a href="#" onclick="myFunction()" class="dropbtn">Sales</a>
+                      <div id="myDropdown" class="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                      </div>
+                  </div>
+                 
+                    
+                
                 <a class="underline" href="#"><div id="menu1" class="menu1">Purchase</div></a>
                 <a class="underline" href="#"><div id="menu1" class="menu1">Supplier</div></a>
                 <a class="underline" href="#"><div id="menu1" class="menu1">Inventory</div></a>
@@ -25,5 +36,26 @@
         </div>
   
   </body>
+<script>
+  /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+  </script>
 </html>
