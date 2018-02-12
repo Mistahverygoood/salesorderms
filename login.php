acd4b8ec32a902577dbafa1dac9a7c304fb299d1
MistahVeryGood <khurrissa@hotmail.com>
2018-02-12 19:11:10 +0800

    updated file

---
+++ login.php
@@ -0,0 +1,32 @@
+<html>
+    <title></title>
+         <link rel="stylesheet" type="text/css" href="css/Style.css">
+         
+    <head></head>
+<body id="bgclLN">
+    
+               
+    <div id="logBan"></div>
+                           
+    <div id="LogBox">
+        <form method="post">
+        <div id="Stxt"><p>Sign in</p></div>
+        <div id="Allreq"><p>All fields are required</p></div>
+        <div id="UnPbx">
+            <input type="text" name="uname" placeholder="Username or Email" required>
+            <input type="password" name="psw" placeholder="Password" required>
+        </div>
+        
+        <div id="subbtn">
+            <input type="submit" name="btn_login" value="Log in">
+            
+        </div>
+        <div id="Rpswrd">
+            <p>Forgot <a href="Recover_Password.php">password</a> ?</p>
+        </div>
+        </form>
+    </div>                 
+
+    
+</body>
+</html>
