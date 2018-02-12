60e5371e932b06ea167d0cff756b2b4ecc6f45cb
MistahVeryGood <khurrissa@hotmail.com>
2018-02-12 20:02:08 +0800

    updated file

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Home</title>
+  </head>
+  <body>
+<!--    -->
+    <div id="">
+      <div><h2>Sales Order Management System</h2></div>
+      <div><p>Username</p></div>
+      <div><p>Account Type</p></div>
+    </div>
+<!--    -->
+    <div id="nav">
+            <div id="navwrapper">
+                <a class="underline" href="#"><div id="menu1" class="menu1">Dashboard</div></a>
+                <a class="underline" href="#"><div id="menu1" class="menu1">Customers</div></a>
+                <a class="underline" href="#"><div id="menu1" class="menu1">Sales</div></a>
+                <a class="underline" href="#"><div id="menu1" class="menu1">Purchase</div></a>
+                <a class="underline" href="#"><div id="menu1" class="menu1">Supplier</div></a>
+                <a class="underline" href="#"><div id="menu1" class="menu1">Inventory</div></a>
+                <a class="underline" href="#"><div id="menu1" class="menu1">Shipping</div></a>
+                <a class="underline" href="#"><div id="menu1" class="menu1">Reports</div></a>
+            </div>        
+        </div>
+  
+  </body>
+
+</html>
